7beaa1dbd802def49ad5ec1a3c937b090b30fe42
Hack-peru <97358615+Hack-peru@users.noreply.github.com>
2023-10-10 19:25:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@
 echo
 echo "Atualizando bot, aguarde..."
 echo
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/index.js > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/veri.js > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/gerar.js > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/index.js > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/veri.js > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/gerar.js > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
 chmod +x qrcode
 mv qrcode /bin
 mv index.js /etc/megahbot
@@ -32,12 +32,12 @@ apt install unzip -y > /dev/null 2>&1
 apt install screen -y > /dev/null 2>&1
 apt install wget -y > /dev/null 2>&1
 wget https://raw.githubusercontent.com/endblack/WaBot-VendasSSH/main/mult/onbot > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1
 chmod +x onbot offbot qrcode
 mv onbot offbot qrcode /bin
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
+wget https://github.com/Hack-peru/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
 unzip /etc/megahbot.zip
 mv megahbot /etc
 echo
